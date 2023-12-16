9ff221e9a42d3791c4929f1c6e5e1f9b3d484998
AleixSaPa <sapafamilia@gmail.com>
2023-12-16 19:04:58 +0100

    Carga Rapida 01

--- Porcentaje.js
+++ Porcentaje.js
@@ -1,4 +1,6 @@
 function P(Identificaccion) {
-    if (Identificaccion == 1) { alert('4%') }
-    else if (Identificaccion == 2) { alert('15%') }
+    if (Identificaccion === 1) { alert('4%') } //Pollo Revvozado de la mama
+    else if (Identificaccion === 2) { alert('15%') }//pastanaga amb patatas
+    else if (Identificaccion === 3) { alert('60%') }//Truita
+    else if (Identificaccion === 3) { alert('55%') }//butifara mab patatas
 }
