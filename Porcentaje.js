e0b67a39ad131418802a5f50f9ff499d961f35a4
AleixSaPa <sapafamilia@gmail.com>
2023-12-16 17:40:10 +0100

    Poner las provavilidades de la comida que e echo

--- Porcentaje.js
+++ Porcentaje.js
@@ -1,6 +1,4 @@
-functionP()
-{
-    var Identificaccion = functionP
-    if (Identificaccion == 1) { alert('Ai Un 4% DE Provavilidades De Que Lo Haga La Mama') }
-    ifelse(2)
+function P(Identificaccion) {
+    if (Identificaccion == 1) { alert('4%') }
+    else if (Identificaccion == 2) { alert('15%') }
 }
